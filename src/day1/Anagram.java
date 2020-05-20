7fee3b68e787acac98be3237294f8d2c3e76d687
tekemehmet <tekemehmet@yahoo.com>
2020-05-20 17:11:06 -0400

    first commit

---
+++ src/day1/Anagram.java
@@ -0,0 +1,31 @@
+package day1;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+
+        String a = "listen";
+        String b = "silent";
+
+        a = a.toLowerCase().replace(" ","");
+        b = b.toLowerCase().replace(" ","");
+
+        if(a.length() != b.length()){
+            System.out.println("Not Anagram");
+            System.exit(0);
+        }
+
+        for (int i = 0; i < a.length(); i++) {
+
+            b= b.replaceFirst(""+ a.charAt(i), "");
+
+        }
+        if (b.isEmpty()){
+            System.out.println("Anagram");
+        }else{
+            System.out.println("Not Anagram");
+        }
+
+
+    }
+}
